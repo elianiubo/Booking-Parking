3a75427bdf89a02cf766e456ada4697003917dd5
Elia <elia.niubo@gmail.com>
2025-03-16 18:20:33 +0100

    Primer commit configuracio feta

---
+++ OneDrive/Carpeta IOC-OneDrive/Asignatures/M12-Projecte/UF1/3-Iteracio inicial de porjecte/ventacar/src/test/java/grup1/ventacar/VentacarApplicationTests.java
@@ -0,0 +1,13 @@
+package grup1.ventacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentacarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
